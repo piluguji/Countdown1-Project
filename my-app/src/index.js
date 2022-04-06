cf2a52c51d426bc2aad723b0d8be996dff8af03b
Aaryan Dhore <30392950+piluguji@users.noreply.github.com>
2022-04-05 23:11:30 -0400

    .

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 function Square(props){
    
     return (
-    <button className="square" onClick={() => props.onClick()}>
+    <button className="square" onClick={props.onClick}>
         {props.value}
     </button>
     );
@@ -17,13 +17,17 @@ function Square(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            XisNext: true,
         };
     }
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.XisNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            XisNext: !this.state.XisNext,
+        });
     }
 
     renderSquare(i) {
